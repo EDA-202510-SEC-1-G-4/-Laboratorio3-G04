bd2c9a00ff80b6d45d5048ddae6baf87c6134e3c
nicolascatsano1107 <n.castanoc2@github.com>
2025-02-07 07:45:20 -0500

    Release clase

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -5,3 +5,22 @@ def new_list1():
     
     }
     return newlist
+
+def get_element(list, index):
+    
+        return list['elements'][index]
+    
+def is_present(my_list, element, compare_function):
+    
+    size = my_list['size']
+    if size > 0:
+        keyexists = False
+        for keypos in range (0, size):
+            info = my_list['elements'][keypos]
+            if compare_function(element, info) == 0:
+                keyexists = True
+                break
+        if keyexists:
+            return keypos
+    return element in list['elements']
+    
