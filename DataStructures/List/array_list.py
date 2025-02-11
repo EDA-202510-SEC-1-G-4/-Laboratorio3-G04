981ad3fddbfa4a13b6e4dd4b0aac6e6edfb2ea90
JD061905 <jecheverryr23@gmail.com>
2025-02-11 08:35:52 -0500

    Funciones array

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -6,9 +6,24 @@ def new_list():
     }
     return newlist
 
+def add_first(list, element):
+    list['elements'].insert(0, element)
+    list['size'] += 1
+    return list
+
+def add_last(list, element):
+    list['elements'].append(element)
+    list['size'] += 1
+    return list
+
+def size(list):
+    return list['size']
+
+def first_element(list):
+    if list['size'] != 0:
+        return list['elements'][0]
+
 def get_element(list, index):
-    
-    
         return list['elements'][index]
     
 def is_present(my_list, element, compare_function):
