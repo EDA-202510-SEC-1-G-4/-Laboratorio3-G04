adc696dd6f78cc8e5add1ca1f1e8470e0e56dec6
nicolascatsano1107 <n.castanoc2@github.com>
2025-02-11 08:53:01 -0500

    cambios_2_implementacion

--- App/logic.py
+++ App/logic.py
@@ -29,7 +29,7 @@ import os
 
 # TODO Importar la librería para el manejo de listas
 
-from DataStructures.List import single_linked_list as lt
+from DataStructures.List import single_linked_list as lista_enlazada
 from DataStructures.List import array_list as lt
 data_dir = os.path.dirname(os.path.realpath('__file__')) + '/Data/'
 
